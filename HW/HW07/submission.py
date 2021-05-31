3df3d6fb89469084e8259192237ce45470eac2c4
beta-lux <beta.underalpha@gmail.com>
2021-06-01 00:22:44 +0900

    4b

--- HW/HW07/submission.py
+++ HW/HW07/submission.py
@@ -282,7 +282,13 @@ class ParticleFilter(object):
     ############################################################
     def elapseTime(self):
         # BEGIN_YOUR_ANSWER (our solution is 7 lines of code, but don't worry if you deviate from this)
-        raise NotImplementedError  # remove this line before writing code
+        result = collections.Counter()
+        for (row, col), value in {key: value for key, value in self.particles.items() if key in self.transProbDict}.items():
+            result.update(dict(collections.Counter(
+                [util.weightedRandomChoice(self.transProbDict[(row, col)]) for _ in range(value)]
+            )))
+
+        self.particles, _ = collections.defaultdict(int, result), self.updateBelief()
         # END_YOUR_ANSWER
 
     # Function: Get Belief
